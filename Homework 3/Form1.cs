d6b3900cf5d034b9ecc102102349854596255136
Trenton Bergdahl <trenton.bergdahl@wsu.edu>
2023-09-19 13:04:32 -0700

    Redid Homewrok 3 Test Project

---
+++ Homework 3/Form1.cs
@@ -0,0 +1,61 @@
+namespace Homework_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Options_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void loadFirst50FibonacciNumbersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //implement
+        }
+
+        private void loadFirst100FibonacciNumbersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //implement
+        }
+
+        private void loadFromFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //implement
+        }
+
+        private void saveToFileToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //implement
+        }
+    }
+}
