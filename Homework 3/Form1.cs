9703fde11cafcec18ce9702bfe50a8585b47f862
Trenton Bergdahl <trenton.bergdahl@wsu.edu>
2023-09-19 14:17:23 -0700

    Implemented saving text to file button

--- Homework 3/Form1.cs
+++ Homework 3/Form1.cs
@@ -78,10 +78,33 @@ namespace Homework_3
         /// <param name="e"></param>
         private void saveToFileToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            //implement
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog1.FileName;
+                
+                using (FileStream outfile = new FileStream(filePath, FileMode.Create))
+                {
+                    using(StreamWriter streamWriter = new StreamWriter(outfile))
+                    {
+                        streamWriter.WriteLine(textBox1.Text);
+                    }
+                }
+            }
         }
 
-
-       
+        /// <summary>
+        /// Reads all text from sr and puts it in private member textBox1
+        /// </summary>
+        /// <param name="sr"></param>
+       private void LoadText(TextReader sr)
+       {
+            
+            string line, text_box_contents = "";
+            while ((line = sr.ReadLine()) != null)
+            {
+                text_box_contents += line;
+            }
+            textBox1.Text = text_box_contents;
+        }
     }
-}
+}     
